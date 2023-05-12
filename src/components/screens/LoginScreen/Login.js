8b0c03a40365ed34d16ef2e575027a0bc596b448
wasswaenockmale <enockmale97@gmail.com>
2023-05-12 11:52:18 +0300

    Cleaned up and styled the login screen once again

--- src/components/screens/LoginScreen/Login.js
+++ src/components/screens/LoginScreen/Login.js
@@ -2,40 +2,41 @@ import { Text,
          View,
          TextInput,
          StatusBar,
+         TouchableOpacity
 } from "react-native"
 
-import { style } from "./style"
+import { LoginStyle } from "./style"
 import { COLORS } from "../../contants/contants"
 import { AntDesign } from "@expo/vector-icons"
 
-export function Login(){
+export default function Login(){
     return(
-        <View style={style.container}>
-            <View style={style.header}>
-                <AntDesign name="left" size={24} color="white" style={style.leftBtn} 
-                            onPress={() => {
-                                props.navigation.navigate('Home');
-                            }}
+        <View style={LoginStyle.container}>
+            <View style={LoginStyle.header}>
+                <AntDesign name="left" size={24} color="white" style={LoginStyle.leftBtn} 
+                            onPress={() => alert("Going back to the dashboard")}
                             />
-                <Text style={style.loginText}>Welcome to Ruboni</Text>
+                <Text style={LoginStyle.loginText}>Welcome to Ruboni</Text>
             </View>
-            <View style={style.formView}>
-                <Text style={style.loginHeader}>Login</Text>
-                <View style={style.logoView}>
+            <View style={LoginStyle.formView}>
+                <Text style={LoginStyle.loginHeader}>Login</Text>
+                <View style={LoginStyle.logoView}>
                     {/* I should place a logo for Ruboni here  */}
                 </View>
-                <TextInput style={style.inputField} placeholderTextColor={COLORS.PRIMARY} placeholder=""></TextInput>
-                <TextInput style={style.inputField} placeholderTextColor={COLORS.PRIMARY} placeholder=""></TextInput>
+                <View style={LoginStyle.loginDetails}>
+                    <TextInput style={LoginStyle.inputField} placeholderTextColor={COLORS.PRIMARY} placeholder="Phonenumber"></TextInput>
+                    <TextInput style={LoginStyle.inputField} placeholderTextColor={COLORS.PRIMARY} placeholder="Password"></TextInput>
 
-                <Touchable style={style.loginBtn}>
-                    <Text style={style.login}>Login</Text>
-                </Touchable>
-                <View>
-                    <Text style={style.forgot}>Forgot Password</Text>
-                </View>
-                <View style={style.noAccountSection}>
-                    <Text style={style.noAccount}>Don't have an account? </Text>
-                    <Text style={style.signUp}>SignUp</Text>
+                    <TouchableOpacity style={LoginStyle.loginBtn}>
+                        <Text style={LoginStyle.login}>Login</Text>
+                    </TouchableOpacity>
+                    <View>
+                        <Text style={LoginStyle.forgot}>Forgot Password</Text>
+                    </View>
+                    <View style={LoginStyle.noAccountSection}>
+                        <Text style={LoginStyle.noAccount}>Don't have an account? </Text>
+                        <Text style={LoginStyle.signUp}>SignUp</Text>
+                    </View>
                 </View>
             </View>
             <StatusBar style="auto"/>
--- src/components/screens/LoginScreen/style.js
+++ src/components/screens/LoginScreen/style.js
@@ -11,26 +11,25 @@ export const LoginStyle = StyleSheet.create({
             width: '100%',
             height: '10%',
             flexDirection: 'row',
+            alignItems: "center"
         },
         leftBtn: {
-            width: 25,
-            height: 10,
-            color:COLORS.WHITE,
+            marginRight: 60
         },
         loginText:{
             fontSize: 25,
             color: COLORS.WHITE,
-            textAlign: "center",
         },
         loginHeader:{
-            fontSize: 15,
+            fontSize: 20,
+            marginTop: 10,
             textAlign: "center",
             color: COLORS.PRIMARY,
         },
         logoView:{
             // This is a view that will contain an image of the company's logo 
-            width: 50,
-            height: 50,
+            width: 100,
+            height: 100,
             borderRadius: 25,
             borderWidth: 2,
             borderColor: COLORS.PRIMARY_2,
@@ -39,11 +38,21 @@ export const LoginStyle = StyleSheet.create({
             width:'100%',
             height:'90%',
             backgroundColor: COLORS.WHITE,
+            padding: 5,
+            // justifyContent:"center"
+            alignItems: "center",
+            // borderWidth:5,
+            // borderColor:COLORS.BLACK
         },
         logoView:{
             width: 50,
             height: 50,
             alignSelf:'center',
+        },
+        loginDetails: {
+            width: '90%',
+            height: '70%',
+            alignItems:'center'
         },
         inputField: {
             // TextInput field for login section 
@@ -51,14 +60,18 @@ export const LoginStyle = StyleSheet.create({
             height:40,
             padding: 10,
             borderWidth:1,
+            margin: 20,
             borderColor: COLORS.PRIMARY,
+            borderRadius: 10,
         },
         loginBtn:{
             // Login button space 
             width: 142,
             height: 36,
             backgroundColor: COLORS.PRIMARY,
+            padding: 2,
             borderRadius: 100,
+            margin: 5,
         },
         login:{
             // Text in loginBtn 
@@ -74,6 +87,8 @@ export const LoginStyle = StyleSheet.create({
             flexDirection: "row",
             width: 246,
             height: 19,
+            justifyContent: "center",
+            margin:20,
         },
         noAccount:{
             fontSize: 15,
