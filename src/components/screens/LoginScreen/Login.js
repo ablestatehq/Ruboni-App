1876f4dd34ff739bf25da247949084e57ed4594b
wasswaenockmale <enockmale97@gmail.com>
2023-05-12 10:26:07 +0300

    UI for Login a third way to completion

--- src/components/screens/LoginScreen/Login.js
+++ src/components/screens/LoginScreen/Login.js
@@ -1,14 +1,41 @@
-import { View } from "react-native"
-import { style } from "./style"
+import { Text,
+         View,
+         TextInput,
+} from "react-native"
 
-const Login = () => {
+import { style } from "./style"
+import { COLORS } from "../../contants/contants"
+import { AntDesign } from "@expo/vector-icons"
+
+export function Login(){
     return(
         <View style={style.container}>
-            <View>
-
+            <View style={style.header}>
+                <AntDesign name="left" size={24} color="white" style={style.leftBtn} 
+                            onPress={() => {
+                                props.navigation.navigate('Home');
+                            }}
+                            />
+                <Text style={style.loginText}>Welcome to Ruboni</Text>
             </View>
-            <View>
-                
+            <View style={style.formView}>
+                <Text style={style.loginHeader}>Login</Text>
+                <View style={style.logoView}>
+                    {/* I should place a logo for Ruboni here  */}
+                </View>
+                <TextInput style={style.inputField} placeholderTextColor={COLORS.PRIMARY} placeholder=""></TextInput>
+                <TextInput style={style.inputField} placeholderTextColor={COLORS.PRIMARY} placeholder=""></TextInput>
+
+                <Touchable style={style.loginBtn}>
+                    <Text style={style.login}>Login</Text>
+                </Touchable>
+                <View>
+                    <Text style={style.forgot}>Forgot Password</Text>
+                </View>
+                <View style={style.noAccountSection}>
+                    <Text style={style.noAccount}>Don't have an account? </Text>
+                    <Text style={style.signUp}>SignUp</Text>
+                </View>
             </View>
         </View>
     )
--- src/components/screens/LoginScreen/style.js
+++ src/components/screens/LoginScreen/style.js
@@ -1,25 +1,87 @@
 import { StyleSheet } from "react-native";
-import { COLORS } from "../../contants/contants";
+import { COLORS, DIMENS } from "../../contants/contants";
 
 export const LoginStyle = StyleSheet.create({
         container:{
-            
+            width: DIMENS.SCREENWIDTH,
+            height: DIMENS.SCREENHEIGHT,
+            backgroundColor: COLORS.PRIMARY,
         },
-
+        header:{
+            width: '100%',
+            height: '10%',
+            flexDirection: 'row',
+        },
+        leftBtn: {
+            width: 25,
+            height: 10,
+            color:COLORS.WHITE,
+        },
+        loginText:{
+            fontSize: 25,
+            color: COLORS.WHITE,
+            textAlign: "center",
+        },
+        loginHeader:{
+            fontSize: 15,
+            textAlign: "center",
+            color: COLORS.PRIMARY,
+        },
         logoView:{
-
+            // This is a view that will contain an image of the company's logo 
+            width: 50,
+            height: 50,
+            borderRadius: 25,
+            borderWidth: 2,
+            borderColor: COLORS.PRIMARY_2,
         },
         formView:{
-
+            width:'100%',
+            height:'90%',
+            backgroundColor: COLORS.WHITE,
+        },
+        logoView:{
+            width: 50,
+            height: 50,
+            alignSelf:'center',
         },
         inputField: {
-            width:'90%',
-            height:25,
+            // TextInput field for login section 
+            width:'80%',
+            height:40,
+            padding: 10,
             borderWidth:1,
             borderColor: COLORS.PRIMARY,
         },
-        placeHolder: {
-            
+        loginBtn:{
+            // Login button space 
+            width: 142,
+            height: 36,
+            backgroundColor: COLORS.PRIMARY,
+            borderRadius: 100,
+        },
+        login:{
+            // Text in loginBtn 
+            fontSize: 20,
+            color: COLORS.WHITE,
+            textAlign: "center",
+        },
+        forgot:{
+            // If the user forgot password
+            textAlign: "center"
+        },
+        noAccountSection:{
+            flexDirection: "row",
+            width: 246,
+            height: 19,
+        },
+        noAccount:{
+            fontSize: 15,
+            color: COLORS.PRIMARY,
+        },
+        signUp:{
+            fontSize: 15,
+            color: COLORS.PRIMARY_1,
         }
     }
 )
