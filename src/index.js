fd9b1f4368b8ea40e4a01c5a4884b97bd50876b0
wasswaenockmale <enockmale97@gmail.com>
2023-05-12 12:09:21 +0300

    room cleaning (relocated index.js file

--- src/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Login } from './components/screens/LoginScreen/Login'
-export { default as SignUp } from './components/screens/SignUpScreen/SignUp'
-export { default as Dashboard } from './components/screens/Dashboard/Dashboard'
-export { default as Profile } from './components/screens/Account/Profile'
-export { default as Accommodation } from './components/screens/Accommodations/AccView'
-export { default as ListAccommodation } from './components/screens/Accommodations/List_of_accommodations'
-export { default as Payment } from './components/screens/Payment/Pay'
