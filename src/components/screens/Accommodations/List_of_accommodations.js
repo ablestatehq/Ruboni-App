b8d463eae53c3bceb3b92a6c36844f93cb8baf85
wasswaenockmale <enockmale97@gmail.com>
2023-05-24 12:36:02 +0300

    preparing accommodation view for a given hotel selected

--- src/components/screens/Accommodations/List_of_accommodations.js
+++ src/components/screens/Accommodations/List_of_accommodations.js
@@ -1,17 +1,43 @@
-import { Text, View } from "react-native";
+import { FlatList, StatusBar, Text, View } from "react-native";
 import { AccommodationStyle } from "./style";
 import { AntDesign } from "@expo/vector-icons";
+import { tours } from '../../../dataTours/tours'
+import { COLORS } from "../../contants/contants";
+import ItemCard from '../../helper/ItemCard'
 
 export default function ListAccommodation() {
+    const data = tours
+    // console.log(data)
     return(
         <View>
-            <View style={AccommodationStyle}>
-                <AntDesign name="left" size={24} color="white" onPress={() => alert("Going back to the dashboard")}/>
-                <Text>Accommodations</Text>
+            <View style={AccommodationStyle.listItemsView}>
+                <AntDesign name="left" size={24} color={COLORS.WHITE} onPress={() => alert("Going back to the dashboard")}/>
+                <Text style={AccommodationStyle.listHeaderText}>Hotels</Text>
             </View>
-            <View>
+            <View style={AccommodationStyle.list}>
                 {/* Here, list the accommodations available here  */}
+                {/* <View>
+                    User can select the type of room from here.
+                </View> */}
+                <FlatList 
+                    style={AccommodationStyle.listItems}
+                    data={data}
+                    renderItem={({item}) => {
+                       return <ItemCard 
+                       ItemCard={AccommodationStyle.itemCard}
+                       touchStyle={AccommodationStyle.touchStyle}
+                       url={item.image}
+                       imgStyle={AccommodationStyle.imgStyle}
+                       itemDes={AccommodationStyle.itemDes}
+                       description={item.description}
+                       itemFooter={AccommodationStyle.itemFooter}
+                       itemF={AccommodationStyle.itemF}
+                       price={item.price} />
+                    }}
+                    showsVerticalScrollIndicator={false}
+                />
             </View>
+            <StatusBar barStyle='light-content'/>
         </View>
     )
 }
--- src/components/screens/Accommodations/style.js
+++ src/components/screens/Accommodations/style.js
@@ -1,6 +1,153 @@
 import { StyleSheet } from "react-native";
+import { COLORS } from "../../contants/contants";
 
 export const AccommodationStyle = StyleSheet.create({
-    // List of accommodations styles.
-    
+    // accView.js file styles. below
+    aContainer:{
+        // margin:10,
+        flex:1,
+        alignItems:'center'
+    },
+    header:{
+        // padding:5,
+        // position: "absolute",
+        margin:10,
+        height:'30%',
+        width:'95%',
+        borderRadius:10,
+        backgroundColor:'blue',
+    },
+    zindexView:{
+        width:'90%',
+        height:'70%',
+        backgroundColor:COLORS.WHITE,
+        borderRadius:10,
+        borderWidth:2,
+        marginTop:-50,
+        borderColor:COLORS.WHITE_1,
+        padding:10,
+        // alignItems:'center'
+    },
+    adetail:{
+        width:'90%',
+        height:'40%',
+        marginTop:10,
+        // padding:5,
+        borderRadius:5,
+        borderWidth:1,
+        borderColor:COLORS.WHITE_1
+    },  
+    selectPackage:{
+        width:'90%',
+        height:"20%",
+        marginTop:10,
+    },
+    aMap:{
+        height:'20%'
+    },
+    aBuy:{
+        flexDirection:'row',
+        justifyContent:"space-between",
+        alignItems:'center',
+        padding:10
+    },
+    bookBtn:{
+        backgroundColor:COLORS.PRIMARY,
+        borderRadius:10,
+        padding:5,
+    },
+    btnText:{
+        color:COLORS.WHITE,
+        textAlign:"center"
+    },
+    accScroll:{
+        // borderWidth:1,
+        height:'100%', 
+        width:'100%',
+        padding:5,
+    }, // End of accView.js file styles
+
+
+    // make reservation component style below
+    mContainer:{
+        width:'100%',
+        height:'100%',
+    },
+    mHeader:{
+        width:'100%',
+        height:'10%',
+        backgroundColor: COLORS.PRIMARY,
+        flexDirection: 'row',
+        padding:5,
+        alignItems:'center',
+
+    },
+    mHText:{
+        fontSize:20,
+        color:COLORS.WHITE,
+        marginLeft:50,
+        textAlign:'center',
+    },
+    mTextInput:{
+        
+    },
+    mContent:{
+        width:'100%',
+        padding:20,
+        flexDirection:'column',
+        alignItems:"center"
+    },
+    mTextInput:{
+        width:'100%',
+        borderWidth:1,
+        borderColor:COLORS.PRIMARY,
+        height:50,
+        marginBottom:10,
+    },
+    mDatePicker:{
+        backgroundColor:COLORS.PRIMARY_1,
+        margin:10,
+        height:50,
+        justifyContent:'center',
+        width:'90%'
+    },
+    mText:{
+        textAlign:"center",
+    },
+    mbtnText:{
+        color:COLORS.WHITE,
+        textAlign:'center'
+    },
+    mSection:{
+        backgroundColor: COLORS.PRIMARY_2,
+        width:'100%',
+        height:100,
+    },
+    mProceed:{
+        borderRadius:10,
+        backgroundColor:COLORS.PRIMARY,
+        width:'50%',
+        height:30,
+        alignItems:'center',
+        justifyContent:'center',
+        margin:10,
+    }, // End of reserversation.js file styles below.
+
+    //List_of_accommodations.js file styles below
+    listItemsView:{
+        backgroundColor:COLORS.PRIMARY,
+        flexDirection:"row",
+        height:'15%',
+        alignItems:"center"
+    },
+    listHeaderText:{
+        textAlign:"center",
+        fontSize:20,
+        width:'80%',
+        color:COLORS.WHITE,
+    },
+    list:{
+        flexDirection:'column',
+        alignItems:"center"
+    }
 });
