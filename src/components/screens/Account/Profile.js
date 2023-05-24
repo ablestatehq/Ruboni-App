123cdf1f80b357981851f5fd444ce4f34d0fd4ce
wasswaenockmale <enockmale97@gmail.com>
2023-05-24 13:09:16 +0300

    setting up user profile page

--- src/components/screens/Account/Profile.js
+++ src/components/screens/Account/Profile.js
@@ -1,3 +1,29 @@
-export function Profile(params) {
-    
+import {
+    View,
+} from 'react-native';
+import Btn from '../../helper/Btn';
+import { profileStyle } from './style';
+
+export default function Profile(props) {
+    return(
+        <View>
+            <View style={profileStyle.header}>
+
+            </View>
+            <View style={profileStyle.cardBtn}>
+                <Btn 
+                    BtnStyle={profileStyle.transparentBtn}
+                    BtnText="Notification" />
+                <Btn 
+                    BtnStyle={profileStyle.transparentBtn}
+                    BtnText="Payment methods" />
+                <Btn 
+                    BtnStyle={profileStyle.transparentBtn}
+                    BtnText="Settings" />
+                <Btn 
+                    BtnStyle={profileStyle.logout}
+                    BtnText="Log out" />
+            </View>
+        </View>
+    )
 }
--- src/components/screens/Account/style.js
+++ src/components/screens/Account/style.js
@@ -1,5 +1,5 @@
 import { StyleSheet } from "react-native";
 
-export const AccountStyles = StyleSheet.create({
+export const profileStyle = StyleSheet.create({
 
 });
