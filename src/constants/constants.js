c603678d2561db6f628885da43f9f6439156ad55
wasswaenockmale <enockmale97@gmail.com>
2023-06-02 10:01:00 +0300

    renamed the contants folder to constants folder

---
+++ src/constants/constants.js
@@ -0,0 +1,28 @@
+import { Dimensions } from "react-native"
+
+export const COLORS = {
+    PRIMARY:'#82AAE3',
+    PRIMARY_1:'#BFEAF5',
+    PRIMARY_2:'#EAFDFC',
+    WHITE: '#fff',
+    WHITE_1:'#F9F8F8',
+    BLACK: '#000',
+    GREEN:'00FF00'
+}
+
+export const DIMENS = {
+    FORM: {
+        PADDING: 20,
+    },
+    SCREENHEIGHT: Dimensions.get('screen').height,
+    SCREENWIDTH: Dimensions.get('screen').width,
+}
+
+export const FONTS = {
+    SIZE: {
+        HEADING: 20,
+        NORMAL: 1.2,
+        SMALLEST: 7
+    },
+    
+}
