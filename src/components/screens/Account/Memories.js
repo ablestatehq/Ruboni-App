a9232bd41efebf0766c9c837e55b0793d80630eb
wasswaenockmale <enockmale97@gmail.com>
2023-06-14 14:07:05 +0300

    image-picker works find

--- src/components/screens/Account/Memories.js
+++ src/components/screens/Account/Memories.js
@@ -7,17 +7,38 @@ import {
 } from 'react-native';
 import { profileStyle } from './style';
 import { AntDesign } from '@expo/vector-icons';
-import { COLORS } from '../../contants/contants';
+import { COLORS } from '../../../constants/constants';
 import { tours } from '../../../dataTours/tours';
 import Trips from '../../helper/Trips';
+import { useState } from 'react';
+import * as ImagePicker from 'expo-image-picker';
 
-export default function Memory(props){
+export default function Memory({navigation}){
+    const [ image, setImage ] = useState(null);
+
+    const pickImage = async ()=>{
+        let results = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: false,
+            aspect: [4,3],
+            quality: 1,
+            allowsMultipleSelection:true
+        });
+
+        console.log(results);
+
+        if(!results.canceled){
+            setImage(results.assets[0].uri);
+        }else{
+            console.log("Canceled")
+        }
+    }
     return(
         <View style={profileStyle.mContainer}>
             <View style={profileStyle.mheader}>
-                <AntDesign name='left' size={32} color={COLORS.WHITE} onPress={() => props.navigation.navigate('Account')}/>
+                <AntDesign name='left' size={32} color={COLORS.WHITE} onPress={() => navigation.navigate('Profile')}/>
                 <Text style={profileStyle.mHeaderText}>Memories</Text>
-                <AntDesign name='plus' size={32} color={COLORS.WHITE} onPress={() => alert('add new images')} />
+                <AntDesign name='plus' size={32} color={COLORS.WHITE} onPress={pickImage} />
             </View>
             <View style={profileStyle.mContent}>
                 <View style={profileStyle.mImage}>
